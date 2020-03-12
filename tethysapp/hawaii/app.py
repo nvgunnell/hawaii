f9b754fcd02b7c37a9a1b58d35b4aca65cc173f4
nvgunnell <tsax111@gmail.com>
2020-03-12 13:04:48 -0600

    first

---
+++ tethysapp/hawaii/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Hawaii(TethysAppBase):
+    """
+    Tethys app class for Hawaii.
+    """
+
+    name = 'Hawaii'
+    index = 'hawaii:home'
+    icon = 'hawaii/images/icon.gif'
+    package = 'hawaii'
+    root_url = 'hawaii'
+    color = '#f39c12'
+    description = ''
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='hawaii',
+                controller='hawaii.controllers.home'
+            ),
+        )
+
+        return url_maps
